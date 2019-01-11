4a19031aa8c7fc75e4772f886aae970f2e0c2f05
eschoenborn <eschoenborn@magicwriter.com>
2019-01-11 14:46:30 -0500

    GH-17 - step1: Write Migrations for checklist_template_user. Store them in /dbm Include a couple example records we can use for priming the database

---
+++ dbm/migrations.sql
@@ -0,0 +1,11 @@
+--Checklist_Template_User migrations
+CREATE SCHEMA IF NOT EXISTS ChecklistDojo;
+CREATE TABLE IF NOT EXISTS ChecklistDojo.Checklist_Template_User(
+  ID SERIAL NOT NULL PRIMARY KEY ,
+  UserID SERIAL NOT NULL,
+  Name VARCHAR(72) NOT NULL,
+  Json JSON NOT NULL
+);
+--Example Records
+INSERT INTO ChecklistDojo.Checklist_Template_User (UserID, Name, Json)
+VALUES (01,'ExampleTable', '{ "Title": "ExampleTable","Description": "Doing a thing","Items": {"Text": "thing","Selected": true}}');
