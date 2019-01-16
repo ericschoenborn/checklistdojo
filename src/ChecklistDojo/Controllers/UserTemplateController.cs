726edd3eb882c206d43134697fab9ccc1f26e3e9
eschoenborn <eschoenborn@magicwriter.com>
2019-01-16 17:38:53 -0500

    GH-17 added some content to UserTemplateController

--- src/ChecklistDojo/Controllers/UserTemplateController.cs
+++ src/ChecklistDojo/Controllers/UserTemplateController.cs
@@ -1,19 +1,22 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ChecklistDojo.Services;
 
 namespace ChecklistDojo.Controllers
 {
     [Route("~/ChecklistTemplate")]
     public class UserTemplateController : Controller
     {
-        public UserTemplateController()
+        private IUserTemplateService UserTemplateService { get; }
+        public UserTemplateController(IUserTemplateService userTemplateService)
         {
-
+            UserTemplateService = userTemplateService;
         }
 
         [HttpGet]
         public async Task<IActionResult> GetUserTemplates()
         {
+            var (userTemplate, error) = await UserTemplateService.GetUserTemplates().ConfigureAwait(false);
             return Json("{content}");
         }
     }
