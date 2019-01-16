1817dc0e146ac3be48f16c85d18990f5353b59b6
eschoenborn <eschoenborn@magicwriter.com>
2019-01-16 16:32:32 -0500

    GH-17 - basic UserTemplateRepository setup

--- src/ChecklistDojo/Data/Repositories/UserTemplateRepository.cs
+++ src/ChecklistDojo/Data/Repositories/UserTemplateRepository.cs
@@ -1,4 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using ChecklistDojo.Data.Models;
+
 namespace ChecklistDojo.Data.Repositories
 {
+    public interface IUserTemplateRepository
+    {
+        Task<List<UserTemplate>> GetUserTemplates();
+    }
 
+    public class UserTemplateRepository : IUserTemplateRepository
+    {
+        public UserTemplateRepository()
+        {
+
+        }
+        public async Task<List<UserTemplate>> GetUserTemplates()
+        {
+            return new List<UserTemplate>{};
+        }
+    }
 }
