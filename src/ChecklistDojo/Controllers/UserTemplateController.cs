543cc38e4f54bbf5997242e1ef5b9c2a1b6beeaa
eschoenborn <eschoenborn@magicwriter.com>
2019-01-16 16:17:47 -0500

    GH-17 - basic controller set up

--- src/ChecklistDojo/Controllers/UserTemplateController.cs
+++ src/ChecklistDojo/Controllers/UserTemplateController.cs
@@ -1,3 +1,20 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
 namespace ChecklistDojo.Controllers
 {
+    [Route("~/ChecklistTemplate")]
+    public class UserTemplateController : Controller
+    {
+        public UserTemplateController()
+        {
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUserTemplates()
+        {
+            return Json("{content}");
+        }
+    }
 }
