c4515ebf78186cb8b837e74dd90a0e77047b534d
eschoenborn <eschoenborn@magicwriter.com>
2019-01-16 11:53:49 -0500

    GH-17 - basic setup for UserTemplateService.cs

--- src/ChecklistDojo/Services/UserTemplateService.cs
+++ src/ChecklistDojo/Services/UserTemplateService.cs
@@ -1,4 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using ChecklistDojo.Data.Models;
+
 namespace ChecklistDojo.Services
 {
+    public interface IUserTemplateService
+    {
+        Task<(List<UserTemplate>, string)> GetUserTemplates();
+    }
 
+    public class UserTemplateService : IUserTemplateService
+    {
+        public UserTemplateService()
+        {
+
+        }
+
+        public async Task<(List<UserTemplate>, string)> GetUserTemplates()
+        {
+            return (null, "error");
+        }
+
+    }
 }
